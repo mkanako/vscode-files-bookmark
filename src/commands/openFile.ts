1e935fab4c21cf24890e84516c74e8fb7eb3576a
kanako <4190346+mkanako@users.noreply.github.com>
2020-07-31 09:25:52 +0800

    up

--- src/commands/openFile.ts
+++ src/commands/openFile.ts
@@ -1,21 +1,14 @@
-import { workspace, window, Uri } from 'vscode'
+import { window, Uri, commands } from 'vscode'
 
 export default {
   identifier: 'openFile',
   async handler (args: string | Record<string, 'path'>) {
     try {
       const path = typeof args === 'string' ? args : args.path
-      let document
-      if (path.startsWith('vscode-userdata:')) {
-        document = await workspace.openTextDocument(Uri.parse(path))
-      } else {
-        document = await workspace.openTextDocument(path)
-      }
-      if (document) {
-        await window.showTextDocument(document, {
-          preview: typeof args === 'string',
-        })
-      }
+      const uri = path.startsWith('vscode-userdata:') ? Uri.parse(path) : Uri.file(path)
+      await commands.executeCommand('vscode.open', uri, {
+        preview: typeof args === 'string',
+      })
     } catch (error) {
       window.showErrorMessage(error.message)
     }
